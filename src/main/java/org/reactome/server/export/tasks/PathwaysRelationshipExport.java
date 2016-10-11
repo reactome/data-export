54214b319ae3c40094c04e9ad734ec91e0825db3
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-10-11 13:50:26 +0100

    lists pathways relationship of those available in the hierarchy of the different species

---
+++ src/main/java/org/reactome/server/export/tasks/PathwaysRelationshipExport.java
@@ -0,0 +1,35 @@
+package org.reactome.server.export.tasks;
+
+import org.neo4j.ogm.model.Result;
+import org.reactome.server.export.annotations.DataExport;
+import org.reactome.server.export.tasks.common.DataExportAbstract;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ */
+@SuppressWarnings("unused")
+@DataExport
+public class PathwaysRelationshipExport extends DataExportAbstract {
+
+    @Override
+    public String getName() {
+        return "ReactomePathwaysRelation";
+    }
+
+    @Override
+    public String getQuery() {
+        //All pathways reachable from TopLevelPathway which have at least one pathway in the hasEvent slot
+        return " MATCH (p:Pathway)-[:hasEvent]->(sp:Pathway) " +
+                "WHERE (p)<-[:hasEvent*]-(:TopLevelPathway) OR (p:TopLevelPathway) " +
+                "RETURN DISTINCT p.stId AS P_Identifier, sp.stId AS SP_Identifier, p.speciesName AS Species " +
+                "ORDER BY p.speciesName, p.stId, sp.stId";
+    }
+
+    @Override
+    public void printResult(Result result, Path path) throws IOException {
+        print(result, path, false, "P_Identifier", "SP_Identifier");
+    }
+}
