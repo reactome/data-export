9fe449e4bf2725c28bf7fb6562018b01914d702f
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-02-13 12:17:30 +0000

    removing the session caching

--- src/main/java/org/reactome/server/export/config/ReactomeNeo4jConfig.java
+++ src/main/java/org/reactome/server/export/config/ReactomeNeo4jConfig.java
@@ -1,15 +1,12 @@
 package org.reactome.server.export.config;
 
 import org.neo4j.ogm.config.Configuration;
-import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.reactome.server.graph.config.Neo4jConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -23,11 +20,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableNeo4jRepositories( basePackages = {"org.reactome.server.graph.repository"} )
 @EnableSpringConfigured
-public class GraphQANeo4jConfig extends Neo4jConfig {
+public class ReactomeNeo4jConfig extends Neo4jConfig {
     private static final Logger logger = LoggerFactory.getLogger("importLogger");
 
     private SessionFactory sessionFactory;
-    private Session session;
 
     @Bean
     public Configuration getConfiguration() {
@@ -47,18 +43,6 @@ public class GraphQANeo4jConfig extends Neo4jConfig {
             sessionFactory = new SessionFactory(getConfiguration(), "org.reactome.server.graph.domain" );
         }
         return sessionFactory;
-    }
-
-    @Override
-    @Bean
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public Session getSession() throws Exception {
-        if (session == null){
-            logger.info("Opening neo4j Session");
-            session = super.getSession();
-        }
-
-        return session;
     }
 
 }
