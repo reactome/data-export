d2ef488583c446093ed312ced9640822987a2b44
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-10-09 12:48:10 +0100

    quotes removed for columns content the TSV generation

--- src/main/java/org/reactome/server/export/tasks/common/DataExportAbstract.java
+++ src/main/java/org/reactome/server/export/tasks/common/DataExportAbstract.java
@@ -62,7 +62,7 @@ public abstract class DataExportAbstract implements DataExport {
                 // Some results might be list of elements. In some cases we use REDUCE and the output looks like
                 // ["a", "b", "c", ] and we want it to look like ["a", "b", "c"].
                 //               ^ we remove this comma and the space after it
-                // That's why we replace ", ]" by "]"
+                // That's why we replace ", ]$" by "]"
                 Object aux = map.get(attribute);
                 if(aux instanceof Object[]){
                     StringBuilder rtn = new StringBuilder("[");
@@ -71,7 +71,7 @@ public abstract class DataExportAbstract implements DataExport {
                     }
                     aux = rtn.append("]").toString();
                 }
-                line.add("\"" + (aux == null ? null : ("" + aux).replaceAll(", ]$", "]")) + "\"");
+                line.add((aux == null ? "-" : ("" + aux).replaceAll(", ]$", "]")));
             }
             lines.add(StringUtils.join(line, "\t"));
         }
