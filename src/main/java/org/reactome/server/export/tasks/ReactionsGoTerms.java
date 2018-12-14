481f11a335544502782c36afeaf6349fc170fe2e
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-12-14 16:40:12 +0000

    only reactions for stId starting by "R-HSA-"

--- src/main/java/org/reactome/server/export/tasks/ReactionsGoTerms.java
+++ src/main/java/org/reactome/server/export/tasks/ReactionsGoTerms.java
@@ -6,8 +6,6 @@ import org.reactome.server.export.tasks.common.DataExportAbstract;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
@@ -16,25 +14,15 @@ import java.util.Map;
 @DataExport
 public class ReactionsGoTerms extends DataExportAbstract {
 
-    private static final String HOMO_SAPIENS_TAX_ID = "9606";
-
     @Override
     public String getName() {
         return "Reactions2GoTerms_human";
     }
 
     @Override
-    protected Map<String, Object> getMap() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("taxId", HOMO_SAPIENS_TAX_ID);
-        return map;
-    }
-
-    @Override
     public String getQuery() {
-        return " MATCH (s:Species)<-[:species]-(rle:ReactionLikeEvent)-[:catalystActivity]->(ca:CatalystActivity)-[:activity]->(go:GO_MolecularFunction) " +
-                "WITH DISTINCT rle, go, COLLECT(DISTINCT s) AS species " +
-                "WHERE SIZE(species) = 1 AND SINGLE(x IN species WHERE x.taxId = {taxId}) " +
+        return " MATCH (rle:ReactionLikeEvent)-[:catalystActivity]->(ca:CatalystActivity)-[:activity]->(go:GO_MolecularFunction) " +
+                "WHERE rle.stId CONTAINS 'R-HSA-' " +
                 "RETURN rle.stId AS Identifier, rle.displayName AS Name, go.databaseName + ':' + go.accession AS GO_Term";
     }
 
