ff6f102cfe28cef44f8c7a4449a14c86e71e907e
Joel Weiser <jweiser13@protonmail.com>
2025-01-28 16:38:33 -0500

    updates verifier class to use default verifier

---
+++ src/main/java/org/reactome/server/export/verifier/DataExportVerifier.java
@@ -0,0 +1,20 @@
+package org.reactome.server.export.verifier;
+
+import com.martiansoftware.jsap.*;
+import org.reactome.release.verifier.DefaultVerifier;
+import org.reactome.release.verifier.Verifier;
+
+import java.io.IOException;
+
+/**
+ * @author Joel Weiser (joel.weiser@oicr.on.ca)
+ * Created 1/3/2025
+ */
+public class DataExportVerifier {
+
+    public static void main(String[] args) throws IOException {
+        Verifier verifier = new DefaultVerifier("data_export");
+        verifier.parseCommandLineArgs(args);
+        verifier.run();
+    }
+}
