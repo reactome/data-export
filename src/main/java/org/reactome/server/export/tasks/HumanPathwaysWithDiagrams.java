b6edb5766fefc8c4be15ba6adb3d626da4564f5a
Neil Sachdeva <nsachdeva.iv@gmail.com>
2025-01-29 17:22:39 -0500

    Changed

--- src/main/java/org/reactome/server/export/tasks/HumanPathwaysWithDiagrams.java
+++ src/main/java/org/reactome/server/export/tasks/HumanPathwaysWithDiagrams.java
@@ -22,16 +22,15 @@ public class HumanPathwaysWithDiagrams extends DataExportAbstract {
     @Override
     public String getQuery() {
         return "MATCH (p:Pathway)-[:species]->(:Species {dbId: 48887})\n"+
-        "OPTIONAL MATCH (p)-[:representedPathway]->(diagram:PathwayDiagram)\n"+
         "OPTIONAL MATCH (p)-[:disease]->(d:Disease)\n"+
-        "WITH p, diagram, d IS NOT NULL AS isDisease\n"+
-        "WHERE diagram IS NOT NULL\n"+ 
-        "AND NOT ALL((diagram)-[:hasNode]->(n) WHERE n:GreenBoxDiagram OR n:Compartment)\n"+
-        "WITH p.dbId AS pathwayId, p.displayName AS pathwayName, isDisease\n"+
-        "CALL apoc.export.csv.query(\n" +
-        "\"RETURN $pathwayId AS PathwayID, $pathwayName AS PathwayName, $isDisease AS IsDisease\",\n"+  
-        "'humanPathwaysWithDiagrams.csv',\n" + 
-        "{headers: true}\n) YIELD file\n"+
-        "RETURN 'File exported to: ' + file";
+        "WITH p, d IS NOT NULL AS isDisease\n"+
+        "RETURN p.stId AS pathwayId, p.displayName AS pathwayName, isDisease\n";
     }
+
+    @Override
+    public void printResult(Collection<Map<String, Object>> result, Path path) throws IOException {
+        print(result, path, "pathwayId", "pathwayName", "isDisease");
+    }
+
+    // check if st ids are in the diagram
 }
